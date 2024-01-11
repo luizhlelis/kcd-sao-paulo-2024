b9a4296136f02352887dbb99e29954e88b8410a5
Luiz Lelis <lelis.luiz@belonghome.com>
2024-01-10 22:36:39 -0300

    fix: publishing based on the architecture :art:

---
+++ src/Scripts/publish-app.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+arch=$(uname -m)
+
+echo "### The host architecture is $arch"
+
+if [ "$arch" == "arm64" ]; then
+  echo "### Publishing for arm64"
+  dotnet publish $1 -r linux-arm64 -o bin -c Release
+else
+  echo "### Publishing for amd64"
+  dotnet publish $1 -r linux-x64 -o bin -c Release
+fi
