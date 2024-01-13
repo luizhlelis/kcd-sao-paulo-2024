2c9f732373d697e146de4e728232f33beeed0290
Luiz Lelis <lelis.luiz@belonghome.com>
2024-01-13 18:56:14 -0300

    fix: identifying OS arch ⚙️

--- src/Scripts/publish-app.sh
+++ src/Scripts/publish-app.sh
@@ -4,10 +4,10 @@ arch=$(uname -m)
 
 echo "### The host architecture is $arch"
 
-if [ "$arch" == "arm64" ]; then
+if [ "$arch" == "arm64" ] || [ "$arch" == "aarch64" ]; then
   echo "### Publishing for arm64"
-  dotnet publish $1 -r linux-arm64 -o bin -c Release
+  dotnet publish "$1" -r linux-arm64 -o bin -c Release
 else
   echo "### Publishing for amd64"
-  dotnet publish $1 -r linux-x64 -o bin -c Release
+  dotnet publish "$1" -r linux-x64 -o bin -c Release
 fi
